adce558b81bae69b754ab4d34bd81318176bdc31
mibrhim <cap.eng.mahmoud@gmail.com>
2022-06-12 18:42:42 +0200

    Enhancing Serializer

--- apps/api/v1/serializers.py
+++ apps/api/v1/serializers.py
@@ -6,3 +6,19 @@ class AppSerializer(serializers.ModelSerializer):
     class Meta:
         model = App
         fields = "__all__"
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
+    name = models.CharField(required=True, max_length=50, min_length=1)
+    type = EnumChoiceField(AppTypes, required=True)
+    framework = EnumChoiceField(FrameworkTypes, required=True)
+    domain_name = models.CharField(max_length=50)
+    screenshot = models.CharField(read_only=True)
+
+    class AppTypes(ChoiceEnum):
+        web="Web"
+        mobile="Mobile"
+
+    class FrameworkTypes(ChoiceEnum):
+        django="Django"
+        react="React Native"
+
--- apps/models.py
+++ apps/models.py
@@ -4,14 +4,14 @@ from django.db import models
 
 class App(models.Model):
     "Generated Model"
-    name = models.TextField()
-    description = models.TextField(
-        null=True,
-        blank=True,
+    name = models.TextField(
+        null=False,
+        blank=False,
     )
+    description = models.TextField()
     type = models.TextField(
-        null=True,
-        blank=True,
+        null=False,
+        blank=False,
     )
     framework = models.TextField(
         null=True,
@@ -49,6 +49,5 @@ class App(models.Model):
         blank=True,
         auto_now=True,
     )
-
 
 # Create your models here.
